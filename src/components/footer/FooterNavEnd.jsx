39ca750d0b9492ac491ca7579683ccc628c8730a
gralweb <gralweb21@gmail.com>
2021-05-02 17:33:04 -0430

    actualizando links para que funcione la app en github pages

--- src/components/footer/FooterNavEnd.jsx
+++ src/components/footer/FooterNavEnd.jsx
@@ -5,25 +5,25 @@ const FooterNavEnd = () => (
     <nav className='footer-nav'>
         <ul className='footer-nav-links' >
             <li className='footer-list-item'>
-                <Link to='/seccion1'>Seccion 1</Link>
+                <Link to='/gralweb/seccion1'>Seccion 1</Link>
             </li>
             <li className='footer-list-item'>
-                <Link to='/seccion2'>Seccion 2</Link>
+                <Link to='/gralweb/seccion2'>Seccion 2</Link>
             </li>
             <li className='footer-list-item'>
-                <Link to='/seccion3'>Seccion 3</Link>
+                <Link to='/gralweb/seccion3'>Seccion 3</Link>
             </li>
         </ul>
 
         <ul className='footer-nav-links' >
             <li className='footer-list-item'>
-                <Link to='/seccion4'>Seccion 4</Link>
+                <Link to='/gralweb/seccion4'>Seccion 4</Link>
             </li>
             <li className='footer-list-item'>
-                <Link to='/seccion5'>Seccion 5</Link>
+                <Link to='/gralweb/seccion5'>Seccion 5</Link>
             </li>
             <li className='footer-list-item'>
-                <Link to='/seccion6'>Seccion 6</Link>
+                <Link to='/gralweb/seccion6'>Seccion 6</Link>
             </li>
         </ul>
     </nav>
